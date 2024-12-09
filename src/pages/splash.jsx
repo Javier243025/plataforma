5d5dc2c7bc7e36032e660e743328716a60ded1f2
Javier243025 <>
2024-12-09 15:55:14 -0500

    redirect if is authnticated

--- src/pages/splash.jsx
+++ src/pages/splash.jsx
@@ -1,8 +1,17 @@
 import { useNavigate } from 'react-router-dom'
 import { Loader } from '../components/loader'
+import { useAuth } from '../hooks/use-auth'
+import { use, useEffect } from 'react'
 
 export const SplashPage = () => {
     const navigate = useNavigate()
+    const { userState } = useAuth()
+
+    if(userState === 'AUTHENTICATED') {
+        navigate('/home')
+    } else {
+        navigate('/login')
+    }
 
     return (
         <div className="w3-display-container" style={{height: '100vh'}}>
