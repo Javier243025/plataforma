f90bb93002c32388a88ef502deb4b879c6bce8a7
Javier243025 <>
2024-12-09 15:32:35 -0500

    capture errors

--- src/pages/not-found.jsx
+++ src/pages/not-found.jsx
@@ -1,8 +1,16 @@
+import { Link } from "react-router-dom"
+
 export const NotFoundPage = () => {
     return (
-        <div className="w3-display-container w3-green" style={{height: '100vh'}}>
-            <div className="w3-display-middle">
-                Not found
+        <div className="w3-display-container" style={{height: '100vh'}}>
+            <div className="w3-display-middle w3-center">
+                <h1>
+                    No se encontró lo que buscabas
+                </h1>
+
+                <Link className="w3-button w3-green" to={'/'}>
+                    Ir al inicio
+                </Link>
             </div>
         </div>
     )
