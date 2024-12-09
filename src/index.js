148dc2237366dcc11e48786bd5dcf18052cbb9a4
Javier243025 <>
2024-12-09 15:30:54 -0500

    add react router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { LoginPage } from './pages/login';
+import { HomePage } from './pages/home';
+import { SplashPage } from './pages/splash';
 import reportWebVitals from './reportWebVitals';
 import './styles/w3css/index.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { NotFoundPage } from './pages/not-found';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <LoginPage />
+    <BrowserRouter>
+      <Routes>
+        <Route index path="/" element={<SplashPage/>} />
+        <Route path="/login" element={<LoginPage/>} />
+        <Route path="/home" element={<HomePage/>} />
+        <Route path="*" element={<NotFoundPage/>} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
