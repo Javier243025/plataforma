d45aa150ff4e012f6f4c7b5ee75435deb90582c9
Javier243025 <>
2024-12-09 15:30:43 -0500

    not foudn page

---
+++ src/pages/not-found.jsx
@@ -0,0 +1,9 @@
+export const NotFoundPage = () => {
+    return (
+        <div className="w3-display-container w3-green" style={{height: '100vh'}}>
+            <div className="w3-display-middle">
+                Not found
+            </div>
+        </div>
+    )
+}
